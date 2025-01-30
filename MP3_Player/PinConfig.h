55ecd3d2a951c73e0fa15ae1dc7c408050361cfc
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-01-29 19:51:07 -0700

    Added Some Pins

--- MP3_Player/PinConfig.h
+++ MP3_Player/PinConfig.h
@@ -1,9 +1,31 @@
 #ifndef PINCONFIG_H
 #define PINCONFIG_H
 
+#define ESP_MOSI  23
+#define ESP_MISO  19
+#define ESP_SCK   18
+
 #define SCREEN_DC 2
 #define SCREEN_CS 4
 #define SCREEN_BL 25
+
+#define DETECT_JACK 12
+#define GAIN 26
+#define DAC_DIN  13
+#define DAC_BCLK 14
+#define DAC_LRC  15
+
+#define HOLD 17
+#define HOME_BUTTON
+#define BACK_BUTTON
+#define HOME_BUTTON
+#define HOME_BUTTON
+
+#define MOTOR_DRIVER 22
+
+
+
+
 
 
 #endif
