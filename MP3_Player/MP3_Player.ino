29537934cf972752f61899e1393456cb44181e07
steel-wing <dw395@byu.edu>
2025-01-29 19:58:06 -0700

    Added graphics library includes and example

--- MP3_Player/MP3_Player.ino
+++ MP3_Player/MP3_Player.ino
@@ -1,4 +1,7 @@
 #include <InputManager.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
 
 void setup() {
   // put your setup code here, to run once:
