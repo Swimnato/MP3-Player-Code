12b3db35ffa706ddfd2949d9f3dcb0429f489128
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-02-26 19:40:19 -0700

    changed libraries

---
+++ MP3_Player/PlatformIO/MP3 Player PlatformIO/lib/arduino-audio-tools-main/src/AudioTools/AudioCodecs/CodecHelix.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "AudioTools/AudioCodecs/AudioCodecsBase.h"
+#include "AudioTools/AudioCodecs/CodecAACHelix.h"
+#include "AudioTools/AudioCodecs/CodecMP3Helix.h"
+#include "AudioTools/AudioCodecs/CodecWAV.h"
+
+namespace audio_tools {
+
+/**
+ * @brief MP3 and AAC Decoder using libhelix:
+ * https://github.com/pschatzmann/arduino-libhelix. We dynamically create a MP3
+ * or AAC decoder dependent on the provided audio format. In addition WAV files
+ * are also supported
+ * @ingroup codecs
+ * @ingroup decoder
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
+
+class DecoderHelix : public MultiDecoder {
+ public:
+  DecoderHelix() {
+    // register supported codecs with their mime type
+    multi.addDecoder(mp3, "audio/mpeg");
+    multi.addDecoder(aac, "audio/aac");
+    multi.addDecoder(wav, "audio/vnd.wave");
+  }
+
+ protected:
+  MP3DecoderHelix mp3;
+  AACDecoderHelix aac;
+  WAVDecoder wav;
+};
+
+}  // namespace audio_tools
