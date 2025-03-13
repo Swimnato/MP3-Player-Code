6178abbb26dc61bed845510ae95eb24cff0f602e
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-03-12 19:27:34 -0600

    switched over to cpp Strings

--- MP3_Player/PlatformIO/MP3 Player PlatformIO/src/menu.cpp
+++ MP3_Player/PlatformIO/MP3 Player PlatformIO/src/menu.cpp
@@ -1,28 +1,26 @@
 #include "menu.hpp"
 #include <Adafruit_GFX.h>
 extern Adafruit_ST7789 *tft;
-error_t write_string(uint8_t x, uint8_t y, char* string, uint8_t string_len, uint16_t color, uint16_t background_color) {
+error_t write_string(uint8_t x, uint8_t y, String string, uint16_t color, uint16_t background_color, uint8_t fontSize = 2) {
     tft->setCursor(x, y);
     tft->setTextColor(color, background_color);
-    tft->setTextSize(2);
+    tft->setTextSize(fontSize);
     tft->setTextWrap(false);
-    for (uint8_t i; i++; i<string_len) {
-        
-
-    }
+    tft->setCursor(x,y);
+    tft->print(string);
 
     return ALL_OK;
 }
 
 error_t clear_display() {
     tft->fillScreen(ST77XX_BLACK);
-
     return ALL_OK;
 }
 
-error_t draw_normal_menu_item(uint8_t row, char* string) {
+error_t draw_normal_menu_item(uint8_t row, String string) {
     uint8_t text_y = row * FULL_LINE_HEIGHT + LINE_TEXT_SEPERATION;
     
+    write_string(0,text_y, ST77XX_BLACK, ST77XX_WHITE);
 
     return ALL_OK;
 }
